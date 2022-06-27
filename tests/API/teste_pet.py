167478248c2095f238d56c11d80d9bcb499e25e8
LHLVilar <lh.vilar@gmail.com>
2022-06-27 11:09:45 -0300

    Teste de API da Petstore, da entidade Pet, com os métodos Get e Put

--- tests/API/teste_pet.py
+++ tests/API/teste_pet.py
@@ -29,3 +29,53 @@ def teste_incluir_pet():
     assert corpo_do_resultado_obtido["name"] == pet_nome_esperado
     assert corpo_do_resultado_obtido['category']['name'] == pet_nome_categoria_esperado
     assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_tag_esperado
+
+
+def teste_consultar_pet():
+    # Entrada
+    pet_id = '2958522' # Tem que ser string porque vai no endereço url
+    # Resultados Esperados
+    status_code_esperado = 200
+    pet_id_esperado = 2958522
+    pet_nome_esperado = "Nina"
+    pet_nome_categoria_esperado = "cachorro"
+    pet_tag_esperado = "vacinado"
+
+    # Executa
+    resultado_obtido = requests.get(
+        url=url + '/' + pet_id,
+        headers=headers
+    )
+
+    # Valida
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    assert resultado_obtido.status_code == status_code_esperado
+    assert corpo_do_resultado_obtido["id"] == pet_id_esperado
+    assert corpo_do_resultado_obtido["name"] == pet_nome_esperado
+    assert corpo_do_resultado_obtido['category']['name'] == pet_nome_categoria_esperado
+    assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_tag_esperado
+
+def teste_alterar_pet():
+    # Entradas/Resultado Esperado
+    status_code_esperado = 200
+    pet_id_esperado = 2958522
+    pet_nome_esperado = "Nina"
+    pet_nome_categoria_esperado = "cachorro"
+    pet_tag_esperado = "vacinado"
+    pet_status_esperado = 'pending'
+
+    # Executa/Resultado Obtido
+    resultado_obtido = requests.put(
+        url=url,
+        headers=headers,
+        data=open("C:\\Users\\lhvil\\PycharmProjects\\134inicial\\vendors\\json\\pet2.json")
+    )
+
+    # Valida
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    assert resultado_obtido.status_code == status_code_esperado
+    assert corpo_do_resultado_obtido["id"] == pet_id_esperado
+    assert corpo_do_resultado_obtido["name"] == pet_nome_esperado
+    assert corpo_do_resultado_obtido['category']['name'] == pet_nome_categoria_esperado
+    assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_tag_esperado
+    assert corpo_do_resultado_obtido["status"] == pet_status_esperado
